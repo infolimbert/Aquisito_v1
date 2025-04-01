82fbddf84987d4a7211b266768b8bb038a0f56df
Limbert <infolimberleon@gmail.com>
2025-04-01 15:23:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aquisito_1/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.aquisito_1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.aquisito_1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        }
+    }
